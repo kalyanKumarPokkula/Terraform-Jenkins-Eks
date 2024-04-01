5e0a33b0cd57b2711a56f34c1c8378af88d14dda
kalyan <kalyanKumarPokkula@gmail.com>
2024-04-01 18:56:20 +0530

    added a vpc to eks cluster

--- Eks-Cluster/main.tf
+++ Eks-Cluster/main.tf
@@ -1,22 +1,61 @@
+## vpc 
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.7.0"
+
+  name                 = "abhi-eks-vpc"
+  cidr                 = var.vpc_cidr
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+
+
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
 
   cluster_name    = "my-eks-cluster"
-  cluster_version = "1.27"
+  cluster_version = "1.28"
 
   cluster_endpoint_public_access = true
 
-  vpc_id     = "vpc-072f1897d2bc013ef"
-  subnet_ids = ["subnet-06c73929d801ee8cd","subnet-05b50121332b56e1b"]
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
-    nodes = {
+
+    node_group = {
       min_size     = 1
       max_size     = 3
       desired_size = 2
-
-      instance_type = ["t2.small"]
     }
+  }
+
+  eks_managed_node_group_defaults = {
+    ami_type               = "AL2_x86_64"
+    instance_types         = ["t2.small"]
+    ami_id         = "ami-0a1b648e2cd533174"
+
   }
 
   tags = {
